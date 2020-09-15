21ec6a1f183565d7595321f0b5fc591a484c3e12
kabir-webDev <kabir.webdev@gmail.com>
2020-09-15 21:16:15 +0600

    Setting up Chat footerr

--- src/Component/Chat/Chat.js
+++ src/Component/Chat/Chat.js
@@ -13,9 +13,9 @@ function Chat() {
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar src="https://scontent.fdac99-1.fna.fbcdn.net/v/t1.0-9/33436044_214776529123681_8737639591051264000_n.jpg?_nc_cat=104&_nc_sid=174925&_nc_eui2=AeH15af5Qajd-opaIfrGLCCX3NSdGbo_MPXc1J0Zuj8w9QmJk9sbo1zy2Mj74Bf6r7YHV6Yf5HzxSytC08jm-ZZQ&_nc_ohc=HJjX-Sdoc_sAX-5QVcb&_nc_oc=AQnHukh6fxpgmrsjqXQKjedQsLBoyRC3nqXP3t6p8mWUzowlAJfd3wbYFr26YKeuD9c&_nc_ht=scontent.fdac99-1.fna&oh=d98805fccb1a9f2576ab202166811372&oe=5F86C3E9" />
+        <Avatar src="https://www.vippng.com/png/detail/366-3660934_business-man-business-man-face-png.png" />
         <div className="chat__headerInfo">
-          <h3>Anny</h3>
+          <h3>Tony</h3>
           <p>Last seen at...</p>
         </div>
         <div className="chat__headerRight">
@@ -32,28 +32,13 @@ function Chat() {
       </div>
       <div className="chat__body">
         <p className="chat__message">
-          <span className="chat__name">Anny</span>
-          Tui onek joss
+          <span className="chat__name">Tony</span>
+          Hello bro
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
         <p className="chat__message chat__receiver">
           <span className="chat__name">Kabir</span>
-          Na... Tui Onek Joss
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message">
-          <span className="chat__name">Anny</span>
-          Na... Tui .....
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message chat__receiver">
-          <span className="chat__name">Kabir</span>
-          Tui....
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message">
-          <span className="chat__name">Anny</span>
-          Tui ... Tui ... Tui ...
+          Hey man Wassup!!
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
       </div>
